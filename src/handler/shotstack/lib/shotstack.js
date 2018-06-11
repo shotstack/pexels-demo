d2a4e9f75a0fbdc433380c4298956525de7c6a3d
Jeff Shillitto <jeff@shillitto.com>
2018-06-11 15:35:55 +1000

    Validate initial post data

--- src/handler/shotstack/lib/shotstack.js
+++ src/handler/shotstack/lib/shotstack.js
@@ -1,11 +1,28 @@
 'use strict';
 
+const Joi = require('joi');
 const PexelsAPI = require('pexels-api-wrapper');
 const pexelsClient = new PexelsAPI(process.env.PEXELS_API_KEY);
 
 module.exports.submit = (data) => {
+    const schema = {
+        search: Joi.string().regex(/^[a-zA-Z0-9 ]*$/).min(2).max(30).required(),
+        soundtrack: Joi.string().valid(['disco', 'party', 'melodic']).required(),
+        title: Joi.string().regex(/^[a-zA-Z0-9 ]*$/).min(2).max(30).required()
+    };
+
+    const valid = Joi.validate({
+        search: data.search,
+        soundtrack: data.soundtrack,
+        title: data.title
+    }, schema);
+
     return new Promise((resolve, reject) => {
-        pexelsClient.searchVideos("dog", 5, 1)
+        if (valid.error) {
+            return reject(valid.error);
+        }
+
+        pexelsClient.searchVideos(data.search, 5, 1)
             .then(function(result){
                 console.log(result);
                 return resolve(result);
