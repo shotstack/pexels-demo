913a5a915005ad5e2ca1d4e3194d22c94fdd978a
Jeff Shillitto <jeff@shillitto.com>
2018-07-12 21:12:18 +1000

    Check render job status and play on done
    Refactored and tidied up app.js script

--- web/app.js
+++ web/app.js
@@ -1,50 +1,81 @@
+var apiEndpoint = 'https://dcsgqlt4gd.execute-api.ap-southeast-2.amazonaws.com/dev/shotstack';
+var shotstackOutputUrl = 'https://s3-ap-southeast-2.amazonaws.com/shotstack-dev-output/';
+
+/**
+ * Initialise and play the video
+ *
+ * @param {String} src  the video URL
+ */
+function initialiseVideo(src) {
+    var player = new Plyr('#player');
+
+    player.source = {
+        type: 'video',
+        sources: [
+            {
+                src: src,
+                type: 'video/mp4',
+            }
+        ]
+    };
+
+    $('#player').show();
+    player.play();
+}
+
+/**
+ * Check the render status of the video
+ *
+ * @param {String} id  the render job UUID
+ */
+function pollVideoStatus(id) {
+    $.get(apiEndpoint + '/' + id, function(response) {
+        console.log(response.data);
+        if (!(response.data.status === 'done' || response.data.status === 'failed')) {
+            setTimeout(function () {
+                console.log(id);
+                pollVideoStatus(id);
+            }, 10000);
+        } else if (response.data.status === 'failed') {
+            console.log('render failed');
+        } else {
+            initialiseVideo(shotstackOutputUrl + response.data.owner + '/' + response.data.id + '.mp4');
+        }
+    });
+}
+
+/**
+ * Submit the form with data to create a Shotstack edit
+ */
+function submitVideoEdit() {
+    var formData = {
+        'search': $('#search').val(),
+        'title': $('#title').val(),
+        'soundtrack': $('#soundtrack option:selected').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: apiEndpoint,
+        data: JSON.stringify(formData),
+        dataType: 'json',
+        crossDomain: true,
+        contentType: 'application/json'
+    }).done(function(response) {
+        console.log(response.data);
+        if (response.status !== 'success') {
+            console.log('handle error');
+        } else {
+            pollVideoStatus(response.data.id);
+        }
+    }).fail(function(data) {
+        console.log(data.status);
+    });
+}
+
 $(document).ready(function() {
     $('form').submit(function(event) {
-
-        //$('.form-group').removeClass('has-error'); // remove the error class
-        //$('.help-block').remove(); // remove the error text
-
-        var formData = {
-            'search': $('#search').val(),
-            'title': $('#title').val(),
-            'soundtrack': $('#soundtrack option:selected').val()
-        };
-
-        console.log(formData);
-
-        // process the form
-        $.ajax({
-            type: 'POST',
-            url: 'https://dcsgqlt4gd.execute-api.ap-southeast-2.amazonaws.com/dev/shotstack',
-            data: JSON.stringify(formData),
-            dataType: 'json',
-            crossDomain: true,
-            contentType: 'application/json'
-        })
-        .done(function(data) {
-            console.log(data);
-            if (!data.success) {
-                if (data.errors.name) {
-                    $('#name-group').addClass('has-error'); // add the error class to show red input
-                    $('#name-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
-                }
-                if (data.errors.email) {
-                    $('#email-group').addClass('has-error'); // add the error class to show red input
-                    $('#email-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
-                }
-                if (data.errors.superheroAlias) {
-                    $('#superhero-group').addClass('has-error'); // add the error class to show red input
-                    $('#superhero-group').append('<div class="help-block">' + data.errors.superheroAlias + '</div>'); // add the actual error message under our input
-                }
-            } else {
-                $('form').append('<div class="alert alert-success">' + data.message + '</div>');
-            }
-        })
-        .fail(function(data) {
-            console.log(data.responseText);
-        });
-
+        submitVideoEdit();
         event.preventDefault();
     });
-
 });
