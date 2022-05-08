3e075be52262a78d4bb973deffa4d7fcd4987f75
Jeff Shillitto <jeff@shotstack.io>
2022-05-08 22:35:19 +1000

    Insert space in comment

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-var apiEndpoint = 'http://localhost:3000/demo/shotstack'; //https://3sd8oie0n1.execute-api.ap-southeast-2.amazonaws.com/demo/shotstack';
+var apiEndpoint = 'http://localhost:3000/demo/shotstack'; // https://3sd8oie0n1.execute-api.ap-southeast-2.amazonaws.com/demo/shotstack';
 var progress = 0;
 var progressIncrement = 10;
 var pollIntervalSeconds = 10;
