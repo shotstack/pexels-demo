e609745c9e9745f79899b6074f363509fdb823bb
Jeff Shillitto <jeff@shotstack.io>
2022-05-09 22:14:31 +1000

    Add missing single quote

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-var apiUrl = 'http://localhost:3000/demo/shotstack'; // https://3sd8oie0n1.execute-api.ap-southeast-2.amazonaws.com/demo/shotstack';
+var apiUrl = 'http://localhost:3000/demo/shotstack'; // 'https://3sd8oie0n1.execute-api.ap-southeast-2.amazonaws.com/demo/shotstack';
 var progress = 0;
 var progressIncrement = 10;
 var pollIntervalSeconds = 10;
